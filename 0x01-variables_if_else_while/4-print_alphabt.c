b565325d150fa04daf29db46c5a08263f2f1fd7e
Blessedb1 <blessingetim6667@gmail.com>
2022-10-14 20:41:09 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * print all letters but the letter 'q' and 'e'
+ * Return: Always 0
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		if (c != 'q' && c != 'e')
+			putchar(c);
+		c++;
+	}
+	putchar('\n');
+
+	return (0);
