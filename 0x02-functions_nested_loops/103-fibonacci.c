8c038ea1c8c50b32d67c81440a14a901108e83ef
Blessedb1 <blessingetim6667@gmail.com>
2022-10-19 13:40:30 -0700

    103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -6,18 +6,18 @@
 
 int main(void)
 {
-	long int i, x = 1, y = 2, sum = 0, tsum = 0;
+	long int i, x = 1, y = 2, sum = 0, tSum = 0;
 
 	for (i = 0; i < 49; i++)
 	{
 		if ((y % 2 == 0) && (y <= 4000000))
 		{
-			tsum = tsum + y;
+			tSum = tSum + y;
 		}
 		sum = x + y;
 		x = y;
 		y = sum;
 	}
-	printf("%1d\n", tsum);
+	printf("%1d\n", tSum);
 	return (0);
 }
