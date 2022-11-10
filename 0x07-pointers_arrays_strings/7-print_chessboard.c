14c1e7bce4be184abe5df930d3d7750508e4e13c
Blessedb1 <blessingetim6667@gmail.com>
2022-11-10 06:18:36 -0800

    0x09-static_libraries

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -3,7 +3,7 @@
 /**
  * print_chessboard - prints the chessboard
  * @a: input pointer.
- * Return: no retur
+ * Return: no return
  */
 
 void print_chessboard(char (*a)[8])
---
+++ README.md
@@ -0,0 +1,1 @@
+echo "0x07-pointers arrays strings"
